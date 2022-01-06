3776f0fdd35359e500b235328520a103d2aef56d
Ref-Bit <rsv.gho@gmail.com>
2022-01-06 15:08:15 +0300

    Initialized Signup route with validation

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,9 +1,30 @@
-import { Router } from 'express';
+import { Request, Response, Router } from 'express';
+import { body, validationResult } from 'express-validator';
 
 const router = Router();
 
-router.get('/signup', (req, res) => {
-  res.send('Hi signupRouter!');
-});
+router.post(
+  '/signup',
+  [
+    body('email').isEmail().withMessage('Email must be valid!'),
+    body('password')
+      .trim()
+      .isLength({ min: 8, max: 20 })
+      .withMessage('Password must be between 8 and 20 characters!'),
+  ],
+  (req: Request, res: Response) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json(errors.array());
+    }
+
+    const { email, password } = req.body;
+
+    console.log('Creating User...')
+
+    res.send({});
+  }
+);
 
 export { router as signupRouter };
