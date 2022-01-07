81e52ecebf8de827a40da00504bbc1a9d91727ad
Ref-Bit <rsv.gho@gmail.com>
2022-01-07 10:24:35 +0300

    Changed ingress auth service path

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,7 @@
 import { Request, Response, Router } from 'express';
 import { body, validationResult } from 'express-validator';
-import { DatabaseConnectionError } from '../errors/database-connection-error';
 import { RequestValidationError } from '../errors/request-validator-error';
+import { User } from '../models/User';
 
 const router = Router();
 
@@ -14,7 +14,7 @@ router.post(
       .isLength({ min: 8, max: 20 })
       .withMessage('Password must be between 8 and 20 characters!'),
   ],
-  (req: Request, res: Response) => {
+  async (req: Request, res: Response) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
@@ -23,10 +23,16 @@ router.post(
 
     const { email, password } = req.body;
 
-    console.log('Creating User...');
-    throw new DatabaseConnectionError();
+    const foundedUser = await User.findOne({ email });
 
-    res.send({});
+    if (foundedUser) {
+      return res.json({});
+    }
+
+    const user = new User({ email, password });
+    await user.save();
+
+    res.status(201).json(user);
   }
 );
 
