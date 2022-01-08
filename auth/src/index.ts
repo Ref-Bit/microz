01d81d523ce0d30255307e8f10ed5f9f18ba7e81
Ref-Bit <rsv.gho@gmail.com>
2022-01-08 12:31:06 +0300

    Added JWT based on cookie-session pacakge

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,11 +5,19 @@ import mongoose from 'mongoose';
 import { AuthRouter } from './routes';
 import { errorHandler } from './middlewares/error-handler';
 import { NotFoundError } from './errors/not-found-error';
+import cookieSession from 'cookie-session';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.set('trust proxy', true);
 app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
 app.use(AuthRouter);
 app.all('*', async () => {
   throw new NotFoundError();
@@ -17,6 +25,10 @@ app.all('*', async () => {
 app.use(errorHandler);
 
 const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error('JWT_KEY must be defined');
+  }
+
   try {
     await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
     console.log('Connected to MongoDB...✅');
