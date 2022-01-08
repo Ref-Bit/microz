8b730ad66967af412287428aef06821ef9791e8c
Ref-Bit <rsv.gho@gmail.com>
2022-01-08 12:45:30 +0300

    Added validate-request middleware

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,22 @@
-import { Router } from 'express';
+import { Request, Response, Router } from 'express';
+import { body } from 'express-validator';
+import { validateRequest } from '../middlewares/validate-request';
 
 const router = Router();
 
-router.get('/signin', (req, res) => {
-  res.send('Hi signinRouter!');
-});
+router.post(
+  '/signin',
+  [
+    body('email').isEmail().withMessage('Email must be valid'),
+    body('password')
+      .trim()
+      .notEmpty()
+      .withMessage('Password field is required'),
+  ],
+  validateRequest,
+  (req: Request, res: Response) => {
+    res.json({});
+  }
+);
 
 export { router as signinRouter };
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,9 +1,9 @@
 import { Request, Response, Router } from 'express';
-import { body, validationResult } from 'express-validator';
+import { body } from 'express-validator';
 import { BadRequestError } from '../errors/bad-request-error';
-import { RequestValidationError } from '../errors/request-validator-error';
 import { User } from '../models/User';
 import JWT from 'jsonwebtoken';
+import { validateRequest } from '../middlewares/validate-request';
 
 const router = Router();
 
@@ -28,13 +28,8 @@ router.post(
       .isLength({ min: 8, max: 20 })
       .withMessage('Password must be between 8 and 20 characters!'),
   ],
+  validateRequest,
   async (req: Request, res: Response) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      throw new RequestValidationError(errors.array());
-    }
-
     const { email, password } = req.body;
 
     const user = new User({ email, password });
