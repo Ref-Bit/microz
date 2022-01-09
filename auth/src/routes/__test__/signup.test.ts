1f2e555ea5f128e199fc3347627d63a1d049739a
Ref-Bit <rsv.gho@gmail.com>
2022-01-09 12:15:34 +0300

    Added tests for signup route

---
+++ auth/src/routes/__test__/signup.test.ts
@@ -0,0 +1,60 @@
+import request from 'supertest';
+import { app } from '../../app';
+
+it('Returns a 201 on successful signup', async () => {
+  return request(app)
+    .post('/api/auth/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password',
+    })
+    .expect(201);
+});
+
+it('Returns a 400 with an invalid email', async () => {
+  return request(app)
+    .post('/api/auth/signup')
+    .send({
+      email: 'test.test.com',
+      password: 'password',
+    })
+    .expect(400);
+});
+
+it('Returns a 400 with an invalid password', async () => {
+  return request(app)
+    .post('/api/auth/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'pass',
+    })
+    .expect(400);
+});
+
+it('Returns a 400 with missing email and password', async () => {
+  await request(app)
+    .post('/api/auth/signup')
+    .send({ email: 'test@test.com' })
+    .expect(400);
+  await request(app)
+    .post('/api/auth/signup')
+    .send({ password: 'password' })
+    .expect(400);
+});
+
+it('Disallows duplicated emails', async () => {
+  await request(app)
+    .post('/api/auth/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password',
+    })
+    .expect(201);
+  await request(app)
+    .post('/api/auth/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password',
+    })
+    .expect(400);
+});
