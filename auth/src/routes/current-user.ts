83766c771077a76149596b40bb0ccc83f5bb8a62
Ref-Bit <rsv.gho@gmail.com>
2022-01-09 11:05:35 +0300

    Added requireAuth middleware

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
+import { requireAuth } from '../middlewares/require-auth';
 import { verifyCurrentUser } from '../middlewares/verify-current-user';
 
 const router = Router();
 
-router.get('/current-user', verifyCurrentUser, (req, res) => {
+router.get('/current-user', verifyCurrentUser, requireAuth, (req, res) => {
   res.status(200).json({ currentUser: req.currentUser || null });
 });
 
