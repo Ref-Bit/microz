b20d6799dd142b2d9e3d3dea4490722234993640
Ref-Bit <rsv.gho@gmail.com>
2022-01-13 16:18:18 +0300

    Fixed currentUser null issue

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -5,7 +5,7 @@ import BaseLayout from './components/BaseLayout';
 function App({ Component, pageProps }) {
   return (
     <main className="font-body bg-gray-50 dark:bg-gray-900 text-gray-900 dark:text-gray-50 min-h-screen text-sm">
-      <BaseLayout currentUser={pageProps.data.currentUser}>
+      <BaseLayout currentUser={pageProps?.data?.currentUser}>
         <Toaster
           position="top-center"
           toastOptions={{
