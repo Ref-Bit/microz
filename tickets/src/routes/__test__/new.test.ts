1ea775c197df2396eceae0d846ab012dbf8cfcf3
Ref-Bit <rsv.gho@gmail.com>
2022-01-16 02:06:22 +0300

    Initialized tests for tickets service

---
+++ tickets/src/routes/__test__/new.test.ts
@@ -0,0 +1,8 @@
+import { request } from 'express';
+import { app } from '../../app';
+
+it('Has a route handler listening to /api/tickets for post request', async () => {});
+it('Can only be accessed if user is authenticated', async () => {});
+it('Returns an error if title is invalid', async () => {});
+it('Returns an error if price is invalid', async () => {});
+it('Creates a new ticket with valid input', async () => {});
