ae9b3194146e7fd0102b5fa27817339e25b31d7e
Ref-Bit <rsv.gho@gmail.com>
2022-01-21 00:12:42 +0300

    Added Nats listener class and its subclasses

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,17 +1,23 @@
 import { connect } from 'node-nats-streaming';
+import { randomBytes } from 'crypto';
+import { TicketCreatedListener } from './events/ticket-created-listener';
 
 console.clear();
 
-const stan = connect('ticketing', '123', {
+const stan = connect('ticketing', randomBytes(4).toString('hex'), {
   url: 'http://localhost:4222',
 });
 
 stan.on('connect', () => {
   console.log('Connected to listener');
 
-  const subscription = stan.subscribe('ticket:created');
-
-  subscription.on('message', (msg) => {
-    console.log('Message Received...📩')
+  stan.on('close', () => {
+    console.log('NATS connection is closed ⚰');
+    process.exit();
   });
+
+  new TicketCreatedListener(stan).listen();
 });
+
+process.on('SIGINT', () => stan.close());
+process.on('SIGTERM', () => stan.close());
