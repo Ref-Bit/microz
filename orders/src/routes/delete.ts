7ca109728cbb3b1e5611caef9bf0d9baad379376
Ref-Bit <rsv.gho@gmail.com>
2022-01-24 11:05:54 +0300

    Completed tests for delete order route handler

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,9 +1,44 @@
+import {
+  NotAuthorizedError,
+  NotFoundError,
+  requireAuth,
+  validateRequest,
+} from '@refbit-ticketing/common';
 import { Router, Request, Response } from 'express';
+import { param } from 'express-validator';
+import { Order, OrderStatus } from '../models/Order';
 
 const router = Router();
 
-router.delete('/:orderId', async (req: Request, res: Response) => {
-  res.json({});
-});
+router.delete(
+  '/:orderId',
+  requireAuth,
+  [
+    param('orderId')
+      .not()
+      .isEmpty()
+      .isMongoId()
+      .withMessage('orderId must be provided'),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const order = await Order.findById(req.params.orderId);
+
+    if (!order) {
+      throw new NotFoundError();
+    }
+
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+
+    order.status = OrderStatus.Cancelled;
+    await order.save();
+
+    //TODO: Publish an event Order is Cancelled
+
+    res.status(204).json(order);
+  }
+);
 
 export { router as deleteRouter };
